2ae73b4304044eed4ce7dafd409e2cb6fbf69750
Lex Song <LexSong.tw@gmail.com>
2019-01-20 11:11:28 +0800

    Copy search function from dump_opj.js

--- lat_long_to_octant.js
+++ lat_long_to_octant.js
@@ -5,7 +5,7 @@ const PLANET = 'earth';
 const URL_PREFIX = `https://kh.google.com/rt/${PLANET}/`;
 /****************************************************************/
 
-const { getPlanetoid, getBulk, traverse } = require('./lib/utils')({
+const { getPlanetoid, getBulk, getNode, traverse } = require('./lib/utils')({
 	URL_PREFIX, DUMP_JSON_DIR: null, DUMP_RAW_DIR: null, DUMP_JSON: false, DUMP_RAW: false
 });
 
@@ -26,6 +26,44 @@ async function run() {
 	console.log('-------------')
 
 	const planetoid = await getPlanetoid();
+	const rootEpoch = planetoid.bulkMetadataEpoch[0];
+
+	async function getNodeFromNodePath(nodePath) {
+		let bulk = null, index = -1;
+		for (let epoch = rootEpoch, i = 4; i < nodePath.length + 4; i += 4) {
+			const bulkPath = nodePath.substring(0, i - 4);
+			const subPath = nodePath.substring(0, i);
+
+			const nextBulk = await getBulk(bulkPath, epoch);
+			bulk = nextBulk;
+			index = traverse(subPath, bulk.childIndices);
+			epoch = bulk.bulkMetadataEpoch[index];
+		}
+		if (index < 0) return null;
+		const node = await getNode(nodePath, bulk, index);
+
+		return node;
+	}
+
+	async function search(nodePath, maxLevel=25) {
+		if (nodePath.length > maxLevel) return;
+		try {
+			const node = await getNodeFromNodePath(nodePath);
+			if (node === null) return;
+
+			console.log("found: " + nodePath)
+
+			for (let i = 0; i < 8; i++) {
+				await search(nodePath + "" + i, maxLevel);
+			}
+
+		} catch (ex) {
+			console.error(ex)
+			return;
+		}
+	}
+
+	await search("02", 4);
 }
 
 /****************************************************************/
